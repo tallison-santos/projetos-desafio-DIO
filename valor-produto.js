a44de008042fb363c1cfdc738c34648a1eefe7ec
Tállisson Santos <105830434+tallison-santos@users.noreply.github.com>
2023-07-19 21:33:18 -0300

    Calcular média de notas de alunos.
    Código JavaScript criado com o intuito de calcular a média de notas dos alunos.

---
+++ valor-produto.js
@@ -0,0 +1,12 @@
+const precoEtiqueta = 100;
+const formaDePagamento = 1;
+
+if (formaDePagamento === 1){
+    console.log(precoEtiqueta - (precoEtiqueta * 0.1));
+} else if (formaDePagamento === 2){
+    console.log(precoEtiqueta -(precoEtiqueta * 0.15));
+} else if (formaDePagamento === 3) {
+    console.log(precoEtiqueta);
+} else {
+    console.log(precoEtiqueta + (precoEtiqueta + 0.1));
+}
